38eacbbb588eff1cb538cf8831a44e64e205a394
ElMehdiCherkaoui <cherkaouim451@gmail.com>
2026-09-17 12:10:56 +0100

    change the name of the categories and change the risk values

--- transformation/gold_transformation.py
+++ transformation/gold_transformation.py
@@ -13,7 +13,7 @@ weather_df = pd.read_json(json_path)
 
 
 def categorize_temp(row):
-    if row['temp_max'] > 35:
+    if row['temp_max'] > 45:
         return 'High Risk Heat'
     elif row['temp_min'] < 5:
         return 'Low Risk Cold'
@@ -21,15 +21,15 @@ def categorize_temp(row):
         return 'Optimal'
     
 def categorize_wind(row):
-    if row['wind_speed'] > 20:
+    if row['wind_speed'] > 40:
         return 'High Risk Wind'
-    elif row['wind_gusts'] > 30:
+    elif row['wind_gusts'] > 65:
         return 'High Risk Gusts'
     else:
         return 'Optimal'
 
 def categorize_rain(row):
-    if row['precipitation'] > 50:
+    if row['precipitation'] > 30:
         return 'High Risk Rain'
     elif row['precipitation_probability'] > 80:
         return 'High Risk Probability'
@@ -38,19 +38,19 @@ def categorize_rain(row):
     
 def calculate_risk(row):
     risk_score = 0
-    if row['Temperature_Category'] == 'High Risk Heat':
+    if row['temperature_category'] == 'High Risk Heat':
         risk_score += 35
-    elif row['Temperature_Category'] == 'Low Risk Cold':
+    elif row['temperature_category'] == 'Low Risk Cold':
         risk_score += 1
 
-    if row['Wind_Category'] == 'High Risk Wind':
+    if row['wind_category'] == 'High Risk Wind':
         risk_score += 35
-    elif row['Wind_Category'] == 'High Risk Gusts':
+    elif row['wind_category'] == 'High Risk Gusts':
         risk_score += 1
 
-    if row['Rain_Category'] == 'High Risk Rain':
+    if row['rain_category'] == 'High Risk Rain':
         risk_score += 30
-    elif row['Rain_Category'] == 'High Risk Probability':
+    elif row['rain_category'] == 'High Risk Probability':
         risk_score += 1
 
     return risk_score
@@ -60,11 +60,9 @@ def calculate_risk(row):
     
 
 
-weather_df['Temperature_Category'] = weather_df.apply(categorize_temp, axis=1)
-weather_df['Wind_Category'] = weather_df.apply(categorize_wind, axis=1)
-weather_df['Rain_Category'] = weather_df.apply(categorize_rain, axis=1)
-weather_df['Risk_Score'] = weather_df.apply(calculate_risk, axis=1)
-
-
+weather_df['temperature_category'] = weather_df.apply(categorize_temp, axis=1)
+weather_df['wind_category'] = weather_df.apply(categorize_wind, axis=1)
+weather_df['rain_category'] = weather_df.apply(categorize_rain, axis=1)
+weather_df['risk_score'] = weather_df.apply(calculate_risk, axis=1)
 
 weather_df.to_json(output_path, orient='records', date_format='iso', indent=4)
